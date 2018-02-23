2eaefc9abf54ee42f273352056c65f33fd90c280
Keith James <kdj@sanger.ac.uk>
2018-02-23 14:32:38 +0000

    Fixed the 'build' element to be a dict rather than a list.
    Added parallel building.
    
    Added bcftools 1.7.

--- bcftools/1.7/build.sh
+++ bcftools/1.7/build.sh
@@ -6,5 +6,5 @@ n=`expr $CPU_COUNT / 4 \| 1`
 
 ./configure --prefix="$PREFIX" --with-htslib="$PREFIX"
 
-make -j $n # Workaround https://github.com/samtools/bcftools/issues/727
+make -j $n
 make install prefix="$PREFIX"
