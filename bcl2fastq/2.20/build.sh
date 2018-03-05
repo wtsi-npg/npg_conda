5808b4451aecee769f32110fecd1d7ff36958b53
Keith James <kdj@sanger.ac.uk>
2018-03-05 10:07:19 +0000

    Workaround to build on Ubuntu Xenial where /usr/include/stat.h has been moved to /usr/include/x86_64-linux-gnu.
    Changed build to use it's internally sourced libxml2 and libxslt
    because our Conda-packaged versions of these, while new enough, are
    not the very specific versions that the configure script checks for.

--- bcl2fastq/2.20/build.sh
+++ bcl2fastq/2.20/build.sh
@@ -6,10 +6,15 @@ tar xvfz bcl2fastq2-v2.20.0.422-Source.tar.gz
 mkdir bcl2fastq/build
 cd bcl2fastq/build
 
+# Workaround for sys/stat.h being moved from its usual place in Ubuntu
+# Xenial
+export "C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu"
+
 export CXXFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
 n=`expr $CPU_COUNT / 4 \| 1`
+
 ../src/configure --prefix="$PREFIX" --parallel=$n
 
 make -j $n
