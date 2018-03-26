bdb74a084a78a47e425a147487eae90172899e8a
Keith James <kdj@sanger.ac.uk>
2018-03-26 11:11:43 +0100

    Added libuuid as a dependency and configured the apr-util dependencies specifically with $PREFIX.
    Added missing apr-util sha256.

--- apr-util/1.6.1/build.sh
+++ apr-util/1.6.1/build.sh
@@ -4,7 +4,11 @@ set -e
 
 n=`expr $CPU_COUNT / 4 \| 1`
 
-CC="gcc -m64" ./configure --prefix="$PREFIX" --with-apr="$PREFIX" CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
+CC="gcc -m64" ./configure \
+  --prefix="$PREFIX" \
+  --with-apr="$PREFIX/bin/apr-1-config" \
+  --with-expat="$PREFIX" \
+  CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 
 make -j $n CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install prefix="$PREFIX"
