38ba5bb7e60282a972af4772bea96b1d6b35212a
Keith James <dev@deoxybyte.co.uk>
2018-04-16 11:31:36 +0100

    Split curl into executable and library/header packages. Updated packages that depend on the library package.

--- recipes/curl/7.58.0/build.sh
+++ recipes/curl/7.58.0/build.sh
@@ -4,6 +4,10 @@ set -e
 
 n=`expr $CPU_COUNT / 4 \| 1`
 
-./configure prefix="$PREFIX" --with-zlib="$PREFIX" CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
+./configure prefix="$PREFIX" \
+            --with-gnutls="$PREFIX" \
+            --with-zlib="$PREFIX" \
+            --without-ssl \
+            CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make -j $n
 make install prefix="$PREFIX"
