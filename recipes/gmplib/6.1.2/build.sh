3cfe0c3ad805736803c04b2d0ff6ae980970f814
Keith James <kdj@sanger.ac.uk>
2018-06-15 17:40:34 +0100

    Enable a portable GMP build and run "make check" when building.

--- recipes/gmplib/6.1.2/build.sh
+++ recipes/gmplib/6.1.2/build.sh
@@ -4,6 +4,11 @@ set -e
 
 n=`expr $CPU_COUNT / 4 \| 1`
 
-./configure --prefix="$PREFIX"
+# -march=x86_64 and MPN_PATH are set enable a build product that is
+# -portable across our CPUs
+./configure --prefix="$PREFIX" \
+            CFLAGS="-O2 -pedantic -fomit-frame-pointer -m64 -march=x86-64" \
+            MPN_PATH="x86_64 generic"
 make -j $n
+make check
 make install prefix="$PREFIX"
