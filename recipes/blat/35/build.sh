1f3f93cd6dc756f43902fd20c675be9083dd22ea
Keith James <kdj@sanger.ac.uk>
2018-06-21 13:59:55 +0100

    Added blat utilities to the build.

--- recipes/blat/35/build.sh
+++ recipes/blat/35/build.sh
@@ -8,4 +8,8 @@ mkdir -p "$SRC_DIR/bin"
 make -j $n MACHTYPE=X86_64 BINDIR="$SRC_DIR/bin"
 
 mkdir -p "$PREFIX/bin"
-cp "$SRC_DIR/bin/blat" "$PREFIX/bin/"
+
+for f in blat faToNib faToTwoBit gfClient gfServer \
+              nibFrag pslPretty pslReps pslSort twoBitInfo twoBitToFa; do
+    cp "$SRC_DIR/bin/$f" "$PREFIX/bin/"
+done
