3a73ec07ef2e06c0d91420ea8845d746b18a94fe
Keith James <kdj@sanger.ac.uk>
2018-07-12 12:16:18 +0100

    Support RPM packages built on RHEL.

--- recipes/irods/4.1.12/build.sh
+++ recipes/irods/4.1.12/build.sh
@@ -3,12 +3,24 @@
 set -e
 
 
-function untar_data() {
+untar_data() {
     if [ -e data.tar.gz ]; then
         tar xz --strip-components=2 --directory="$PREFIX" --exclude='*.o' --exclude-backups < data.tar.gz
     elif [ -e data.tar.xz ]; then
         tar xJ --strip-components=2 --directory="$PREFIX" --exclude='*.o' --exclude-backups < data.tar.xz
     fi
+}
+
+unrpm() {
+    local rpm="$1"
+
+    mkdir -p ./tmp
+    pushd tmp
+    rpm2cpio ../"$rpm" | cpio --extract --make-directories
+    popd
+
+    tar cf - -C ./tmp . | tar xv --strip-components=2 --directory="$PREFIX" --exclude='*.o' --exclude-backups
+    rm -r ./tmp
 }
 
 git submodule init && git submodule update
@@ -22,11 +34,25 @@ export TERM=dumb
 
 cd build
 
-ar vx irods-icommands-4.1.12-64bit.deb
-untar_data
+# Ubuntu
+if [ -f irods-icommands-4.1.12-64bit.deb ]; then
+    ar vx irods-icommands-4.1.12-64bit.deb
+    untar_data
+fi
 
-ar vx irods-dev-4.1.12-64bit.deb
-untar_data
+if [ -f irods-dev-4.1.12-64bit.deb ] ; then
+    ar vx irods-dev-4.1.12-64bit.deb
+    untar_data
+fi
+
+# RHEL
+if [ -f irods-icommands-4.1.12-64bit-centos5.rpm ] ; then
+    unrpm irods-icommands-4.1.12-64bit-centos5.rpm
+fi
+
+if [ -f irods-dev-4.1.12-64bit-centos5.rpm ] ; then
+    unrpm irods-dev-4.1.12-64bit-centos5.rpm
+fi
 
 # Fix all the absolute symlinks pointing to /usr/include
 perl -le 'use strict; use File::Basename; foreach (@ARGV) { if (my $f = readlink $_) { if ($f =~ m{^/usr/}sm) { unlink $_; symlink(basename($f), $_) } } }' $PREFIX/include/irods/*.hpp
