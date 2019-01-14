feb2ef2c15b8d5d29a973b2893949304d1bf9ded
Keith James <kdj@sanger.ac.uk>
2019-01-14 11:34:03 +0000

    Ensure CPPFLAGS and LDFLAGS set for configure.

--- recipes/libxml2/2.9.7/build.sh
+++ recipes/libxml2/2.9.7/build.sh
@@ -4,7 +4,8 @@ set -ex
 
 n="$CPU_COUNT"
 
-./configure prefix="$PREFIX" --without-python
+CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" \
+        ./configure prefix="$PREFIX" --without-python
 
 make -j $n CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install
