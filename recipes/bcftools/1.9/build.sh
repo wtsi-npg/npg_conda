2e33412ce67648f86003f73534b4ff3dc2f8a4c7
Keith James <kdj@sanger.ac.uk>
2019-01-14 13:51:04 +0000

    Ensure CPPFLAGS and LDFLAGS set for configure.

--- recipes/bcftools/1.9/build.sh
+++ recipes/bcftools/1.9/build.sh
@@ -4,7 +4,8 @@ set -ex
 
 n="$CPU_COUNT"
 
-./configure --prefix="$PREFIX" --with-htslib="$PREFIX"
+CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" \
+        ./configure --prefix="$PREFIX" --with-htslib="$PREFIX" 
 
 make -j $n CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install prefix="$PREFIX"
