4f881098042c37c0a6ac8763a92d08a2e6d854bc
Keith James <kdj@sanger.ac.uk>
2019-01-14 11:45:59 +0000

    Avoid building Python support.

--- recipes/libxslt/1.1.32/build.sh
+++ recipes/libxslt/1.1.32/build.sh
@@ -4,7 +4,7 @@ set -ex
 
 n="$CPU_COUNT"
 
-./configure prefix="$PREFIX"
+./configure prefix="$PREFIX" --without-python
 
 make -j $n CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install
