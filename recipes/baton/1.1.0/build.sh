e51ff165f60253a3459b7074de1d06307dd431a0
Keith James <kdj@sanger.ac.uk>
2019-01-14 11:10:34 +0000

    Ensure that the correct headers are picked up i.e. baton's dependencies rather than iRODS' version of the same dependencies.

--- recipes/baton/1.1.0/build.sh
+++ recipes/baton/1.1.0/build.sh
@@ -5,9 +5,9 @@ set -ex
 n="$CPU_COUNT"
 
 ./configure --prefix="$PREFIX" --with-irods="$PREFIX" \
-            CPPFLAGS="-I$PREFIX/include/irods" \
+            CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
             LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
 
-make -j $n CPPFLAGS="-I$PREFIX/include/irods" \
+make -j $n CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
      LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
 make install prefix="$PREFIX"
