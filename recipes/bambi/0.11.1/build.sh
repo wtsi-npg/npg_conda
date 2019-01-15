e0a5ff91584dea1502dd4d17e9f8ee70673980c0
Keith James <kdj@sanger.ac.uk>
2019-01-15 09:38:14 +0000

    Patch bambi to build against the htslib shared library, rather than static. (NB. This patch is the reason bambi will report its version as 0.11.1-dirty)

--- recipes/bambi/0.11.1/build.sh
+++ recipes/bambi/0.11.1/build.sh
@@ -6,8 +6,8 @@ autoreconf -fi
 
 n="$CPU_COUNT"
 
-./configure --prefix="$PREFIX" --with-htslib="$PREFIX" \
-            CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
+CPPFLAGS="-I$PREFIX/include" LDFLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib" \
+        ./configure --prefix="$PREFIX" --with-htslib="$PREFIX"
 
 make -j $n CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install prefix="$PREFIX"
