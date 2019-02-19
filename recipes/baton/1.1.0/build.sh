b6cdadc18c245cb58017013953c2917f2352eca4
Keith James <dev@deoxybyte.co.uk>
2019-02-19 14:32:10 +0000

    Refactored compiler environment variables. I was forced to use LD_LIBRARY_PATH during configure to allow autoconf's conftest programs to find Conda-hosted libraries in the build environment. I'm pretty sure I shouldn't have to do this (tried -rpath and -rpath-link too).

--- recipes/baton/1.1.0/build.sh
+++ recipes/baton/1.1.0/build.sh
@@ -4,10 +4,13 @@ set -ex
 
 n="$CPU_COUNT"
 
-./configure --prefix="$PREFIX" --with-irods="$PREFIX" \
-            CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
-            LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
+export LD_LIBRARY_PATH="$PREFIX/lib"
 
-make -j $n CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
+CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
+ LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals" \
+ ./configure --prefix="$PREFIX" --with-irods="$PREFIX"
+
+make -j $n \
+     CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
      LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
 make install prefix="$PREFIX"
--- recipes/tears/1.2.4/build.sh
+++ recipes/tears/1.2.4/build.sh
@@ -4,9 +4,11 @@ set -ex
 
 autoreconf -fi
 
-./configure --prefix="$PREFIX" --with-irods="$PREFIX" \
-            CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
-            LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
+export LD_LIBRARY_PATH="$PREFIX/lib"
+
+CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
+ LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals" \
+              ./configure --prefix="$PREFIX" --with-irods="$PREFIX"
 
 make  CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install prefix="$PREFIX"
