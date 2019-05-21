c88f2a7dddcb48520684a7dc034a4f8a7d034327
jmtcsngr <jmtc@sanger.ac.uk>
2019-05-21 09:54:11 +0100

    add recipe for npg_qc_utils 67.0

---
+++ recipes/npg_qc_utils/67.0/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+n="$CPU_COUNT"
+
+pushd norm_fit
+mkdir -p build
+make -j $n CC="$GCC" \
+     LIBPATH="-L$PREFIX/lib"
+cp ./build/norm_fit "$PREFIX/bin/"
+popd
+
+pushd gt_utils
+mkdir -p build
+make -j $n  CC="$GCC" \
+     CPPFLAGS="-I$PREFIX/include" \
+     LDFLAGS="-L$PREFIX/lib"
+make install installdir="$PREFIX/bin"
+popd
