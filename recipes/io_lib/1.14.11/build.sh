a5c59fa2a40c4d219348d56e2ef9c3f8e7829010
jmtcsngr <jmtc@sanger.ac.uk>
2019-05-22 16:23:11 +0100

    pass all build flags to configure not to make
      affects io_lib 1.14.9 & 1.14.11
      new build for 1.14.11

--- recipes/io_lib/1.14.11/build.sh
+++ recipes/io_lib/1.14.11/build.sh
@@ -4,8 +4,12 @@ set -ex
 
 n="$CPU_COUNT"
 
-./configure --prefix="$PREFIX" --with-zlib="$PREFIX" --with-libcurl="$PREFIX"
+./configure \
+  --prefix="$PREFIX" \
+  --with-zlib="$PREFIX" \
+  --with-libcurl="$PREFIX" \
+  CPPFLAGS="-I$PREFIX/include" \
+  LDFLAGS="-Wl,-rpath-link,$PREFIX/lib -Wl,--disable-new-dtags -L$PREFIX/lib"
 
-make -j $n CPPFLAGS="-I$PREFIX/include" \
-     LDFLAGS="-Wl,-rpath-link,$PREFIX/lib -Wl,--disable-new-dtags -L$PREFIX/lib"
-make install prefix="$PREFIX"
+make -j "$n"
+make install
