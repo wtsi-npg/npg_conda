3a7269623027f47cfa567de54bbab19a02cd0504
David K. Jackson <david.jackson@sanger.ac.uk>
2019-05-29 09:58:20 +0000

    remove use of configure --with-libcurl= and --with-zlib= which conflict with configure LDFLAGS= argument

--- recipes/io_lib/1.14.11/build.sh
+++ recipes/io_lib/1.14.11/build.sh
@@ -6,8 +6,6 @@ n="$CPU_COUNT"
 
 ./configure \
   --prefix="$PREFIX" \
-  --with-zlib="$PREFIX" \
-  --with-libcurl="$PREFIX" \
   CPPFLAGS="-I$PREFIX/include" \
   LDFLAGS="-Wl,-rpath-link,$PREFIX/lib -Wl,--disable-new-dtags -L$PREFIX/lib"
 
