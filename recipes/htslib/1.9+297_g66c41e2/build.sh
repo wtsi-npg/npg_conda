d55bc47da61f1cbc3bc3289af3f5a5358b7fce7f
David K. Jackson <david.jackson@sanger.ac.uk>
2019-08-24 22:07:31 +0000

    add libdeflate (and use in htslib)

--- recipes/htslib/1.9+297_g66c41e2/build.sh
+++ recipes/htslib/1.9+297_g66c41e2/build.sh
@@ -12,7 +12,10 @@ cp aclocal.m4.tmp aclocal.m4
 
 ./configure \
     --prefix="$PREFIX" \
+    --with-libdeflate \
     --enable-libcurl \
+    --enable-s3 \
+    --enable-gcs \
     --enable-plugins \
     CC="$GCC" CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 
