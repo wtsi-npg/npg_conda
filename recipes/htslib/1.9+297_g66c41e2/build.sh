36b41c2174df36fff881047b995ef11ec119e973
David K. Jackson <david.jackson@sanger.ac.uk>
2019-08-27 18:34:16 +0000

    split htslib-plugins into separate conda package (and outputs)

--- recipes/htslib/1.9+297_g66c41e2/build.sh
+++ recipes/htslib/1.9+297_g66c41e2/build.sh
@@ -23,8 +23,3 @@ make -j $n AR="$AR"
 make install prefix="$PREFIX"
 popd
 
-pushd plugins
-make IRODS_HOME="$PREFIX" \
-     CC="$GCC" CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
-make install prefix="$PREFIX"
-popd
