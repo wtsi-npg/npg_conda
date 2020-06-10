345e2a610b96813726ecdafb2fde44b5373b3d81
David K. Jackson <david.jackson@sanger.ac.uk>
2020-06-10 22:40:22 +0000

    tweak to allow baton configure to find irods4.2

--- recipes/baton/1da6bc5/build.sh
+++ recipes/baton/1da6bc5/build.sh
@@ -6,7 +6,7 @@ n="$CPU_COUNT"
 
 autoreconf -i
 
-./configure --prefix="$PREFIX" \
+LD_LIBRARY_PATH=$PREFIX/lib ./configure --prefix="$PREFIX" \
             CFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
             CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/irods" \
             LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib/irods/externals"
