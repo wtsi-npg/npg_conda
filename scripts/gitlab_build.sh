f82d2bfaeb0b4210cbcc9dbb219d50299ea78a67
Michael Kubiak <mk35@sanger.ac.uk>
2021-07-12 10:40:37 +0100

    Set build image uid and gid to safe values for centos-7, remove 'red-recipes' build line (clang is now in the channel)

--- scripts/gitlab_build.sh
+++ scripts/gitlab_build.sh
@@ -9,10 +9,5 @@ rsync -av $2 $3 # $CHANNEL_DIR $BUILD_DIR
 
 ./tools/bin/recipebook --changes origin/$1 recipes | ./tools/bin/build \
  --recipes-dir $CI_PROJECT_DIR --artefacts-dir $3 --conda-build-image $CONDA_IMAGE \
- --build-channel $WSI_CONDA_CHANNEL \
- --verbose --remove-container
-
-./tools/bin/recipebook --changes origin/$1 red-recipes | ./tools/bin/build \
- --recipes-dir $CI_PROJECT_DIR --artefacts-dir $3 --conda-build-image $CONDA_IMAGE \
- --build-channel $WSI_CONDA_CHANNEL conda-forge bioconda \
+ --build-channel $WSI_CONDA_CHANNEL --conda-uid 1001 --conda-gid 32001 \
  --verbose --remove-container
